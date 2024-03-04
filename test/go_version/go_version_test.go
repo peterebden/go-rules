c609d1271cad0bcf9b59cbf7e2e7feee3cb91206
Peter Ebden <peter.ebden@gmail.com>
2024-03-04 16:41:35 +0000

    derp derp derp

--- test/go_version/go_version_test.go
+++ test/go_version/go_version_test.go
@@ -1,6 +1,7 @@
 package go_version_test
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,11 +9,15 @@ import (
 
 func TestForLoopRangeScope(t *testing.T) {
 	// This test should pass with the full Go 1.22 compilation experience
+	var wg sync.WaitGroup
+	wg.Add(10)
 	s := make([]int, 10)
 	for i := range 10 {
 		go func() {
 			s[i] = i
+			wg.Done()
 		}()
 	}
+	wg.Wait()
 	assert.EqualValues(t, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, s)
 }
